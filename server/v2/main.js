c4d9340e7a97428c5fa5538ccb33937a099ce744
William <up822718@myport.ac.uk>
2019-01-29 23:05:35 +0000

    geting ready for image uploading

--- server/v2/main.js
+++ server/v2/main.js
@@ -1,11 +1,19 @@
 const express  = require('express');
 const database  = require('../database.js');
 let bodyParser = require('body-parser')
+let multer  = require('multer')
+let upload = multer({ dest: 'uploads/' })
 
 let router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(bodyParser.json())
+
+router.post('.hodingcell/hodingcell', upload.none(), function (req, res, next) {
+  // req.body contains the text fields
+})
+
+
 
 router.get('/getContent', function (req, res) {
   res.json({"content":database.getContentV2(),"text":database.getText()});
@@ -17,3 +25,14 @@ router.post('/text', function (req, res) {
   res.sendStatus(200);
 })
 module.exports = router
+
+
+async function uploadPicture(req, res) {
+    const retval = await database.uploadPicture(req.file);
+    res.json(retval);
+}
+
+function error(res, msg) {
+  res.sendStatus(500);
+  console.error(msg);
+}
